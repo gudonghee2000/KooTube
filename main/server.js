881ee16612bae5dc157a1e910c176b011edeec5b
koo <gudonghee2000@gmail.com>
2021-10-11 17:08:26 +0900

    feat: 서버에 user, root, video 에 대한 라우터들을 연결했습니다.

--- main/server.js
+++ main/server.js
@@ -1,10 +1,15 @@
 import express from "express";
+import rootRouter from "./router/rootRouter";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 
-
+app.use("/", rootRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listen ${PORT}PORT🏍`);
