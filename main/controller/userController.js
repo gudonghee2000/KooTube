aa81fd87d887187156c3e244439486c4d915e274
koo <gudonghee2000@gmail.com>
2021-10-13 13:29:32 +0900

    feat: 회원가입 기능을 생성했습니다.

--- main/controller/userController.js
+++ main/controller/userController.js
@@ -1,3 +1,5 @@
+import User from "../models/User";
+
 export const getLogin = (req, res) => {
 
 }
@@ -7,11 +9,34 @@ export const postLogin = (req, res) => {
 }
 
 export const getJoin = (req, res) => {
-
+    res.render("join", { pageTitle: "Join" });
 }
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
+    const pageTitle = "Join";
+    const { username, name, email, password, password2 } = req.body;
+    let user = await User.find({ username: username });
+    if (!user) {
+        return res.render("join", { pageTitle, errorMessage: "존재하는 아이디입니다." });
+    }
+    user = await User.find({ email: email });
+    if (!user) {
+        return res.render("join", { pageTitle, errorMessage: "존재하는 이메일입니다." });
+    }
+    if (password !== password2) {
+        return res.render("join", { pageTitle, errorMessage: "비밀번호가 일치하지 않습니다." });
+    }
+    try {
+        await User.create({
+            username,
+            name,
+            email,
+            password
+        });
+        return res.redirect("/")
+    } catch (error) {
 
+    }
 }
 
 export const logout = (req, res) => {
