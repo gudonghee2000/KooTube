b309f5dbc25716189c54c644ac619d34b1f67e09
koo <gudonghee2000@gmail.com>
2021-10-15 18:17:15 +0900

    feat: 사용자 프로필을 편집하는 기능을 생성했습니다.

--- main/controller/userController.js
+++ main/controller/userController.js
@@ -70,12 +70,78 @@ export const see = async (req, res) => {
     return res.render("see", { pageTitle: user.name, user });
 }
 
-export const getEdit = (req, res) => {
-
+export const getEdit = async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+        res.status(404).render("404");
+    }
+    if (String(id) !== String(res.locals.loggedInUser._id)) {
+        res.status(404).render("404");
+    }
+    res.render("edit-profile", { pageTitle: `Edit ${user.name}`, user });
 }
 
-export const postEdit = (req, res) => {
-
+export const postEdit = async (req, res) => {
+    const pageTitle = "edit-profile";
+    const { username, name, email } = req.body;
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (String(id) !== String(res.locals.loggedInUser._id)) {
+        res.status(404).render("404");
+    }
+    let exist = await User.exists({ username });
+    if (exist && user.username !== username) {
+        return res.status(404).render("edit-profile", { pageTitle, errorMessage: "이미존재하는 유저네임입니다.", user });
+    }
+    exist = await User.exists({ email });
+    if (exist && user.email !== email) {
+        return res.status(404).render("edit-profile", { pageTitle, errorMessage: "이미존재하는 이메일입니다.", user });
+    }
+    await User.findByIdAndUpdate(id, {
+        username,
+        name,
+        email,
+    })
+    return res.redirect("/")
 }
 
+export const getChangePassword = async (req, res) => {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+        res.status(404).render("404");
+    }
+    if (String(id) !== String(res.locals.loggedInUser._id)) {
+        res.status(404).render("404");
+    }
+    res.render("changepassword", { pageTitle: `ChangePassword ${user.name}`, user });
+}
+
+export const postChangePassword = async (req, res) => {
+    const {
+        session: {
+            user: { _id, password },
+        },
+        body: { currentpassword, newpassword, newpassword2 }
+    } = req;
+    const ok = await bcrypt.compare(currentpassword, password);
+    if (!ok) {
+        return res.status(400).render("changepassword", {
+            pageTitle: "Change Password",
+            errorMessage: "현재 비밀번호가 일치하지 않습니다."
+        });
+    }
+    if (newpassword !== newpassword2) {
+        return res.status(400).render("changepassword", {
+            pageTitle: "Change Password",
+            errorMessage: "새로운 비밀번호가 일치하지 않습니다."
+        });
+    }
+    const user = await User.findById(_id);
+    user.password = newpassword;
+    await user.save();
+    req.session.user.password = user.password;
+    return res.redirect("/");
+}
 
